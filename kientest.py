1b30b005ad5c6f6bfc528157baf41a253797799e
Nguyễn Trung Kiên <vumihao@gmail.com>
2024-05-17 18:57:32 +0700

    kiendzcuteee

--- kientest.py
+++ kientest.py
@@ -1,34 +1,49 @@
 import easyocr
 import cv2
 
-
 reader = easyocr.Reader(['en']) 
 
 
-result = reader.readtext('C:\\Users\\kiend\\OneDrive\\Desktop\\Nhan dien bien\\hanghoa\\im5.jpg')
+result = reader.readtext('C:\\Users\\kiend\\OneDrive\\Desktop\\hanghoa\\im4.png')
+anh = cv2.imread('C:\\Users\\kiend\\OneDrive\\Desktop\\hanghoa\\im4.png')
+font = cv2.FONT_HERSHEY_SIMPLEX 
 
 
 product_database = {
-    "sữa": ["milk", "milK", "milx", "mi1k"],
-    "bread": ["bread", "breaD", "bre4d", "br3ad"],
+    "Sua": ["milk", "milK", "milx", "mi1k"],
     "bimbim": ["Snack", "snacK", "sn4ck", "sn@ck"],
 }
+
+found = False
 
 
 for detection in result:
     text = detection[1]
     
-  
+
     for product, representations in product_database.items():
         for representation in representations:
             if representation.lower() in text.lower():
-                print(f"Phát hiện sản phẩm: {product}")
-                print(f"Văn bản: {text}")
-                print(f"Vị trí: {detection[0]}")
-                break  
-        else:
-            continue  
-        break  
-    else:
-        continue  
-    break  
+                points = detection[0]
+                x1, y1 = points[0]
+                x3, y3 = points[2]
+                cv2.rectangle(anh, (x1, y1), (x3, y3), (0, 255, 0), 2)
+                
+                display_text = f"Hang Hoa: {product}"
+                text_size = cv2.getTextSize(display_text, font, 1, 2)[0]
+                text_width, text_height = text_size[0], text_size[1]
+                image_width = anh.shape[1]
+                new_x = image_width - text_width - 18  
+                cv2.putText(anh, display_text, (new_x, y1 - 10), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                
+                found = True
+                break  # Dừng vòng lặp khi đã tìm thấy sản phẩm phù hợp
+        if found:
+            break  # Thoát khỏi vòng lặp sản phẩm khi đã tìm thấy sản phẩm phù hợp
+    if found:
+        break  # Thoát khỏi vòng lặp phát hiện khi đã tìm thấy một kết quả phù hợp
+
+# Hiển thị ảnh với kết quả nhận dạng
+cv2.imshow('VMUUUUUUU', anh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
